0d425c0c998e7242738073c66c6880f2d220a880
Aihika37 <aihika.dutta11@gmail.com>
2026-06-26 15:42:20 +0530

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,15 +1,21 @@
 from turtle import Turtle
+
 class Paddle(Turtle):
-    def __init__(self):
-       super().__init__()
-       self.shape("square")
-       self.color("black")
-       self.shapesize(stretch_wid=5, stretch_len=1)
-       self.penup()
+    def __init__(self, x):
+        super().__init__()
+        #intializing the paddle at x
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(x, 0)
 
+    #moving up
     def go_up(self):
-          self.goto(self.xcor(), self.ycor() + 20)
+        if self.ycor() < 200:
+            self.sety(self.ycor() + 20)
 
+    #moving down
     def go_down(self):
-          self.goto(self.xcor(), self.ycor() - 20)
-
+        if self.ycor() > -200:
+            self.sety(self.ycor() - 20)
